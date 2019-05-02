f78303c267bcebf808081bae087d8aaa885d8d5c
unknown <janspawel@gmail.com>
2019-05-02 09:22:48 +0200

    push test message

---
+++ client/src/.env.js
@@ -0,0 +1,9 @@
+DB_PASSWOR = mongodb+srv://mernWeather:mernWeather@cluster0-9umby.mongodb.net/test?retryWrites=true
+
+clientId = cd8aecc4601c7b5738d386d18cc5f2b542fd338074eb6c6a5b0ab6ca358903c6
+
+appId = NEKfgdnivkFddPStIUXC
+
+appCode = 0gi7i6DsTcMLCn_rxdQu1w
+
+map_Box_Token = pk.eyJ1IjoiZmlnYXQiLCJhIjoiY2pvamY5Y252MDRqdzN4bzM0MThmMmh6NCJ9.mcnGin-MSaOR1vXjHH3-ng
